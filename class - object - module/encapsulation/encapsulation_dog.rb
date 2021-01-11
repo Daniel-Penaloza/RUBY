7fa225a909ba40dbb117bcb969f8ccb20577b5e4
Daniel Penaloza <danielpenalozatj@gmail.com>
2021-01-11 13:36:44 -0800

    Encapsulation

---
+++ class - object - module/encapsulation/encapsulation_dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    def initialize(name)
+        @name = name
+    end
+
+    def to_s
+        "My name is #{@name}"
+    end
+
+    private
+    attr_reader :name
+end
+
+
+dog = Dog.new('cofi')
+puts dog.name
