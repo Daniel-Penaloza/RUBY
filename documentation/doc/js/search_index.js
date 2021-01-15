860ac4f12e62c6a3257972c1bc750b42b268ea73
Daniel Penaloza <danielpenalozatj@gmail.com>
2021-01-14 18:12:52 -0800

    Basic formatting

--- documentation/doc/js/search_index.js
+++ documentation/doc/js/search_index.js
@@ -1,1 +1,1 @@
-var search_data = {"index":{"searchIndex":["person","new()","print_name()"],"longSearchIndex":["person","person::new()","person#print_name()"],"info":[["Person","","Person.html","","<p>This class stores information about people.\n"],["new","Person","Person.html#method-c-new","(name)","<p>Crate the person object and store their name\n"],["print_name","Person","Person.html#method-i-print_name","()","<p>Print this person&#39;s name to the screen\n"]]}}
+var search_data = {"index":{"searchIndex":[],"longSearchIndex":[],"info":[]}}
---
+++ documentation/rdoc_formatter.rb
@@ -0,0 +1,26 @@
+#= RDoc Example
+#
+#== This is a heading
+#
+#* First item in a outer list
+#   * First item in a inner list
+#   * Second item in a inner list
+#* Second item in a outer list
+#   * Only item in this inner list
+#
+#== This is a second heading
+#
+#Visit www.rubyinside.com
+#
+#== Test of text formatting features
+#
+#Want to see *bold* or _italic_ text? You can even ember
+#+text that looks like code+ by surrounding it with plus
+#symbols. Indented code will be automatically formatted:
+#
+#   class MyClass
+#       def method_name
+#           puts "test"
+#       end
+#   end
+#
