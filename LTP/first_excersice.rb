c6c59e38993b4984eb6a3924ab55ff8aee2d5502
Daniel Penaloza <danielpenalozatj@gmail.com>
2021-02-11 12:49:30 -0800

    LTP FIRST

---
+++ LTP/first_excersice.rb
@@ -0,0 +1,7 @@
+# Hours in a year.
+puts "The hours in a year are #{24 * 365}"
+# Minutes in a decade.
+puts "The minutes in a decare are #{((60 * 24)  * 365) * 10}"                   
+# Age in seconds.
+puts "The age in seconds #{(((365 * 32) * 24) * 60) * 60}"
+
